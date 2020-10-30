6de4d23c53d2a9bcaf8957ee8d9b782ed1c30d61
@reroes <reroes799@gmail.com>
2020-10-29 23:47:26 -0500

    archivos iniciales

---
+++ Ejercicio01/src/ejercicio01/Ejercicio01.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Resolver
+        
+        // Crear una variable entera llamada edad
+        // Asignar a la variable edad el valor de 20
+        
+        
+        // Crear una variable double llamada promedio
+        // Asignar a la variable promedio el valor de 10.2
+        
+        
+        System.out.println(edad);
+        System.out.println(promedio);
+    }
+    
+}
